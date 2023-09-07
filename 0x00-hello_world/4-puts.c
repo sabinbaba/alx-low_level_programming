f47795581a4156af04f2a7219acf40a454d1673c
root <root@W-C-HACKER.localdomain>
2023-09-07 15:10:49 +0200

    well done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  *main - Printing
- *   *
- *    *Return: Always 0 (Successful)
- *     */*/
+  * main - Printing
+  *
+  * Return: Always 0 (Successful)
+  */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
