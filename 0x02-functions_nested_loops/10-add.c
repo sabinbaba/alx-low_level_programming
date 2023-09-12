ac7e5811d7efbd6d9c70ce98a6e226d87b4cd1ae
root <sabintox19@gmail.com>
2023-09-12 04:10:06 -0700

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry
+ * @a: function parameter
+ * @b: function parameter
+ * Desc:
+ *
+ * Return: Always 0 (Success)
+ */
+int add(int a, int b)
+{
+	int res = a + b;
+
+	return (res);
+}
