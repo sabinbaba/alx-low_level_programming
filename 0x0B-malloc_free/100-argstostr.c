fdaabd07aadff0cc5aff00f4bfe397b3c61f48ef
w-c-hacker <sabintox19@gmail.com>
2023-11-20 07:41:41 -0800

    done

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,37 +1,19 @@
 #include "main.h"
 /**
- * argstostr - prints args
- * @ac: takes in width of grid
- * @av: height of grid
- * Return: the args one line at a time
+ * free_grid - allocates a grid, make space and free space
+ * @grid: takes in width of grid
+ * @height: height of grid
+ * Return: free grid
  */
 
-char *argstostr(int ac, char **av)
+void free_grid(int **grid, int height)
 {
-	char *str;
-	int count = 0, a = 0, b = 0, c = 0;
+	int i;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	while (a < ac)
+	for (i = 0; i < height; i++)
 	{
-		b = 0;
-		while (av[a][b] != '\0')
-		{
-			count++;
-			b++;
-		}
-		a++;
+		free(grid[i]);
 	}
-	count = count + ac + 1;
-	str = malloc(sizeof(char) * count);
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	for (a = 0; a < ac; a++)
-	{
-		for (b = 0; av[a][b] != '\0'; b++)
-		{
-			str[c] = av[a][b];
-			c++;
+
+	free(grid);
+}
